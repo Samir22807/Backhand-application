945b6b30bee914dc66993fc940542a9cb7a04a11
Samir <samirchudasama22@gmail.com>
2026-09-11 23:24:33 +0530

    utils updats

---
+++ src/utils/apierror.js
@@ -0,0 +1,26 @@
+class Apierror extends Error {
+    constructor(
+        statusCode, 
+        message = "Something went wrong",
+        error = [],
+        stack = ""
+    ) 
+    {
+        super(message);
+        this.data = null;
+        this.success = false;
+        this.statusCode = statusCode;
+        this.error = error;
+        this.stack = stack;
+
+
+        if(stack) {
+            this.stack = stack;
+        }else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+
+}
+
+export {Apierror};
---
+++ src/utils/apirespons.js
@@ -0,0 +1,10 @@
+class Apiresponse {
+    constructor(statuscode, message = "Success", data) {
+        this.statuscode = statuscode;
+        this.message = message;
+        this.data = data;
+        this.success = statuscode < 400;
+    }
+}
+
+export { Apiresponse };
